1b3cf40f60b9afeb01bf04f6473bd8a4aa99a205
flaaaps <julianwening4@gmail.com>
2021-02-14 22:38:16 +0100

    :rocket: Update cors middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import * as tag from './utils/tag.js';
+import shortid from 'shortid';
 
 import cors from 'cors';
 require('dotenv/config');
@@ -17,11 +18,11 @@ mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true, useUnifiedTopo
 const app = express();
 
 const whiteLists = process.env.NODE_ENV === 'development' ? ['http://localhost:3000'] : ['https://linkoo.netlify.app/'];
-app.use(cors({ origin: whiteLists }));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send({ success: true, NODE_ENV: process.env.NODE_ENV, origin: whiteLists });
+    res.send({ success: true, NODE_ENV: process.env.NODE_ENV, origin: 'all :3' });
 });
 
 app.post('/register', async (req, res) => {
@@ -35,10 +36,10 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const userId = req.body.userId;
-    console.log(userId, req.body);
-    if (!userId) return res.send({ success: false, message: 'Please provide a user id' });
-    await UserModel.findOne({ identifier: userId }, (err, doc) => {
+    const name = req.body.name;
+    console.log(name, req.body);
+    if (!name) return res.send({ success: false, message: 'Please provide a user id' });
+    await UserModel.findOne({ name: name }, (err, doc) => {
         if (err) return res.send({ success: false, message: 'An unexpected error occurred.' });
         doc = {
             identifier: doc.identifier,
@@ -53,17 +54,19 @@ app.post('/login', async (req, res) => {
 app.post('/messages/create', async (req, res) => {
     const userId = req.body.userId;
     const content = req.body.content;
+    console.log(req.body);
     if (!userId || !content) return res.send({ success: false, message: 'Please provide a "userId" and a "content" field' });
 
     const user = await UserModel.findOne({ identifier: userId });
     if (!user) return res.send({ success: false, message: `User with id ${userId} not found.` });
     const message = new MessageModel({
+        id: shortid.generate(),
         userId,
         content,
     });
     message.save().then((doc) => {
         console.log('Saved message:', doc);
-        res.send({ success: true });
+        res.send({ success: true, message: doc });
     });
 });
 
