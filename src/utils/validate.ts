99d9afecf10c41225ba6863a2ae2e224651db611
flaaaps <julianwening4@gmail.com>
2021-02-16 21:37:28 +0100

    :bug: Validate by name instead of id

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,9 +1,9 @@
 import mongoose, { Model } from 'mongoose';
 import { User, UserModel } from '../models/User.js';
 
-export async function username(id: string, returnDoc: boolean = true) {
-    console.log('[Validate | Username] Starting validation for user with id', id);
-    return await UserModel.findOne({ identifier: id }).then((doc) => {
+export async function username(name: string, returnDoc: boolean = true) {
+    console.log('[Validate | Username] Starting validation for user with id', name);
+    return await UserModel.findOne({ name: name }).then((doc) => {
         console.log('It is', !!doc);
         if (returnDoc) {
             if (!doc) return null;
